cc5fb06cfbd930f566bfb0adeb126a11c3051798
MartyCodeClan <zabbalab@hotmail.com>
2017-05-29 09:38:38 +0100

    minor change

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,6 +1,6 @@
-# def pet_shop_name(pet_shop)
-#   return pet_shop[:name]
-# end
+def pet_shop_name(pet_shop)
+  return pet_shop[:name]
+end
 
 # def total_cash(shop_cash)
 #   return shop_cash[:admin][:total_cash]
